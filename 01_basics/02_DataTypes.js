36ba0378d48bf689b0b8c0df75e2a16b3b0760bd
Aaditya-Ajju <9922008359@klu.ac.in>
2024-09-10 17:06:47 +0000

    datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,9 @@
+let name = "ajju" // string
+let age = 18 // int
+let isLoggedIn = false // boolean
+let state;
+//null - standalone value
+// symbol 
+
+console.table([name,age,isLoggedIn,state])
+console.log(typeof "ajju") // by uing this typeof we can find the data types
