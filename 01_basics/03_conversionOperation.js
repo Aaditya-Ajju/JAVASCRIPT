2cade2c73da0b14236be81b3abdcd57107b54896
Aaditya-Ajju <9922008359@klu.ac.in>
2024-09-10 17:42:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33abc"
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let loggedin=0
+let booleanloggedin = Boolean(loggedin)
+console.log((booleanloggedin))
+
+// 0 -> false , 1->true . ""(emptystrings)->false , string->true
