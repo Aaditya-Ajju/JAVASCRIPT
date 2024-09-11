f1cddad2c7c422ebf2fdcb8965e09ff9d6a5fb15
Aaditya-Ajju <9922008359@klu.ac.in>
2024-09-11 11:04:47 +0000

    comparisons done

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,11 @@
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(typeof null);
+console.log("1">1)  // max time for comparison data type must be same 
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(undefined==0)
+
+// === 
+console.log("2"==2)
