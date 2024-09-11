a9246020ad5ccb1e209f4cc7563bc1428677358b
Aaditya-Ajju <9922008359@klu.ac.in>
2024-09-11 11:05:03 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,11 +2,27 @@ let score = "33abc"
 // console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 let loggedin=0
 let booleanloggedin = Boolean(loggedin)
-console.log((booleanloggedin))
+// console.log((booleanloggedin))
 
 // 0 -> false , 1->true . ""(emptystrings)->false , string->true
+
+
+//    ********************OPERATIONS*****************
+let VALUE = 3;
+let negValue = -VALUE
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " world"
+console.log(str1+str2)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+"2"+2)
+console.log(1+2+"2")
+
