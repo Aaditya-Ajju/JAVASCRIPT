ff7b5f457fe0dfff8298a2b69987e2adc3883551
Aaditya-Ajju <9922008359@klu.ac.in>
2024-09-11 16:27:18 +0000

    nums and maths

---
+++ 01_basics/Nums_Maths.js
@@ -0,0 +1,13 @@
+const score= 400
+const number= new Number(400)
+// console.log(number);
+// console.log(number.toFixed(2))
+console.log(number.toPrecision(2));
+
+//++++++++++  Maths +++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.5))
+console.log(Math.min(1,5,6,4,3))
+console.log((Math.random()*10)+1)
