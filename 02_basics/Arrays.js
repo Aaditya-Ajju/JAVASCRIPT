8cafa9e18ecb1e06d2f4a2c6b31a09ee72e5ddb5
Aaditya-Ajju <9922008359@klu.ac.in>
2024-09-11 18:53:50 +0000

    Array Done

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,22 @@
+const myArr=[0,1,2,3]
+// console.log(myArr)
+
+// myArr.push(6)
+// // console.log(myArr)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(7))
+// console.log(myArr.indexOf(3))
+// console.log(myArr.length)
+
+const newArr= myArr.join()
+console.log(myArr)
+console.log(newArr)
+console.log("A",myArr)
+const my1 = myArr.slice(1,3)
+console.log(my1)
+
+console.log("B",myArr)
+const my2 =myArr.splice(1,3)
+console.log(my2)
