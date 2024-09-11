7ac5b435cd93addfce01c6ed256a4ed2eb17fc4e
Aaditya-Ajju <9922008359@klu.ac.in>
2024-09-11 17:22:23 +0000

    Playing with dates

---
+++ 01_basics/Date_time.js
@@ -0,0 +1,6 @@
+let myDate = new Date()
+console.log(myDate.toLocaleString())
+console.log(myDate.toString())
+let myCreatedDate = new Date(2024,0,23,5,3)
+console.log(myCreatedDate.toLocaleString());
+console.log(Math.floor(Date.now()/1000))
