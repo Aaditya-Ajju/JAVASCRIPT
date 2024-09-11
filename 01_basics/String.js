51734e0a7cbec470c38d6f117508f4e85d01277a
Aaditya-Ajju <9922008359@klu.ac.in>
2024-09-11 13:14:52 +0000

    Strings

---
+++ 01_basics/String.js
@@ -0,0 +1,12 @@
+const name = "Ajju"
+const age = 18
+console.log(name+age)
+console.log(`hey , my name is ${name} and my age is ${age}`)
+const gameName = new String('Aaditya')
+console.log(typeof gameName )
+console.log(gameName.charAt(2))
+
+
+ const myname = " Ajju    "
+ console.log(myname.trim());
+ console.log(myname.toLowerCase())
